22355f4fe07dc7166655f140fe71667f8d01ab05
mike2vandy <mike.vandewege@gmail.com>
2020-02-24 17:43:33 -0700

    initial commit

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ translate.py translates protein coding DNA information into amino acids.
 Usage:  
 `translate.py <DNA.fas> > <PROT.fas>`
 
+
+reverseTranslateAlignment.py reverse translates a protein alignment into a codon alignment.  
+Usage:  
+`reverseTranslateAlignment.py <PROTALN.fas> <DNA.fas> > <CODON.fas>`
+
---
+++ reverseTranslateAlignment.py
@@ -0,0 +1,40 @@
+#! /usr/bin/eny python2.7
+
+import sys
+
+def fasDict(i_f):
+  seq_dict = {}
+  with open(i_f) as infile:
+    for line in infile:
+      line = line.strip()
+      if line.startswith('>'):
+        header = line.split()[0].replace('>', '')
+        seq_dict[header] = ''
+      else:
+        seq_dict[header] += line
+
+    return seq_dict
+
+aa_seqs = fasDict(sys.argv[1], True)
+nucl_seqs = fasDict(sys.argv[2], True)
+
+for head in nucl_seqs:
+  n_seq = nucl_seqs[head]
+  if head in aa_seqs:
+    aligned = aa_seqs[head]
+    raw_aa = aligned.replace('-', '')
+    count = 0
+    nucl_aligned = ''
+    for letter in aligned:
+      if letter == '-':
+        nucl_aligned += '---'
+      elif letter == '?':
+        nucl_aligned += '---'
+        count += 1
+      elif letter == raw_aa[count]:
+        nucl = count * 3
+        nucl_aligned += n_seq[nucl:nucl + 3]
+        count += 1
+    print '>' + head
+    printSeq(nucl_aligned)
+
